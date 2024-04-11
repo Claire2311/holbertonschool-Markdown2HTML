427bd5baf840a92b5fc09f615beb5d45826efb43
Claire2311 <claire.girard@ieseg.fr>
2024-04-11 11:19:43 +0200

    Task 0

--- markdown2html.py
+++ markdown2html.py
@@ -10,11 +10,11 @@ import os.path
 
 if len(sys.argv) <= 2:
     print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-    sys.exit("1")
+    sys.exit(1)
 
 if ".md" in sys.argv[1]:
     path = './'
     check_file = os.path.isfile(path + sys.argv[1])
     if not check_file:
         print(f"Missing {sys.argv[1]}", file=sys.stderr)
-        sys.exit("1")
+        sys.exit(1)
