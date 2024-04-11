7ff56683de143287452216e89d4356dc359c55ae
Claire2311 <claire.girard@ieseg.fr>
2024-04-11 15:13:37 +0200

    Task 1

--- markdown2html.py
+++ markdown2html.py
@@ -10,6 +10,7 @@ Usage: python3 markdown2html.py input.md output.html
 
 import sys
 import os.path
+import re
 
 
 def main():
@@ -25,6 +26,21 @@ def main():
         if not check_file:
             print(f"Missing {sys.argv[1]}", file=sys.stderr)
             sys.exit(1)
+
+    with open(sys.argv[1], 'r') as mdfile, open(sys.argv[2], 'a') as htmlfile:
+        for line in mdfile:
+            if line.startswith('#'):
+                values = re.split(r'(^#+)\s', line.strip())
+                title_level = str(len(values[1]))
+                title = values[2].strip()
+                final_title = ('<h' + title_level + '>' + 
+                               title + '</h' + title_level + '>')
+                htmlfile.write(final_title + '\n')
+
+    with open(sys.argv[2], 'r') as f:
+        data = f.read()
+        with open(sys.argv[2], 'w') as w:
+            w.write(data[:-1])
 
 
 if __name__ == "__main__":
